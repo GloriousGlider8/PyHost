7da96ffa1f3e6c800b55983e31679c1bad465e36
GloriousGlider8 <110254845+GloriousGlider8@users.noreply.github.com>
2023-09-16 09:12:24 +0100

    Update and rename __main__.py to funcs.py

---
+++ funcs.py
@@ -0,0 +1,47 @@
+import keyboard as k
+
+LINE_UP = '\033[1A'
+LINE_CLEAR = '\x1b[2K'
+LINE_UC = LINE_UP + LINE_CLEAR
+
+def upAndClear():
+    print(LINE_UP, end=LINE_CLEAR)
+
+def selPrompt(options, icons, intro):
+    os.system("cls")
+    print(intro)
+    print("\nUse [UP] and [DOWN] to move cursor.\nUse [RIGHT] to select.\n")
+
+    print("[" + icons[0] + "] " + options[0])
+
+    for i in range(len(options) - 1):
+        print("[ ] " + options[i + 1])
+
+    temp = 0
+    temp1 = 0
+
+    while True:
+        time.sleep(0.15)
+
+        if temp1 != temp:
+            for _ in range(len(options)):
+                upAndClear()
+            for i in range(len(options)):
+                if temp == i:
+                    print("[" + icons[i] + "] " + options[i])
+                else:
+                    print("[ ] " + options[i])
+                
+            temp1 = temp
+
+        if k.is_pressed("up"):
+            if temp > 0:
+                temp = temp - 1
+                                    
+        elif k.is_pressed("down"):
+            if temp < len(options) - 1:
+                temp = temp + 1
+
+        elif k.is_pressed("right"):
+            os.system("cls")
+            return temp
