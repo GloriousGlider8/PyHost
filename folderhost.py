063e5d290224d37eab533bc1cb30622debd905c7
GloriousGlider8 <110254845+GloriousGlider8@users.noreply.github.com>
2023-09-30 11:07:32 +0100

    Security Update #2

--- folderhost.py
+++ folderhost.py
@@ -2,6 +2,12 @@ import http.server
 import socket
 import socketserver
 import os
+import yaml
+import colorama as c
+
+hostyml = open("host.yml")
+hostSettings = yaml.load(hostyml.read(), Loader=yaml.Loader)
+hostyml.close()
 
 PORT = int(input("Port number: "))
 os.environ['USERPROFILE']
@@ -18,6 +24,14 @@ IP = "http://" + s.getsockname()[0] + ":" + str(PORT)
 link = IP
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    if hostSettings["denylist"].find(str(Handler.client_address)) and hostSettings["use"] == "deny":
+            Handler.send_error(403, "Blocked IP", "Ask the host to unblock " + str(Handler.client_address) + " in their host.yml file or PyHost settings.")
+            print(c.Fore.YELLOW + "Blocked IP (" + str(Handler.client_address) + ") attempted to connect!" + c.Style.RESET_ALL)
+
+    if not hostSettings["allowlist"].find(str(Handler.client_address)) and hostSettings["use"] == "allow":
+        Handler.send_error(403, "Non-whitelisted IP", "Ask the host to allow " + str(Handler.client_address) + " in their host.yml file or PyHost settings.")
+        print(c.Fore.YELLOW + "Non-whitelisted (" + str(Handler.client_address) + ") attempted to connect!" + c.Style.RESET_ALL)
+
     print("\n")
     print("Serving at port", PORT)
     print("Type this in your Browser", IP)
